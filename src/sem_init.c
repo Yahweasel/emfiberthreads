e251111736c7bfa7df108c8c154a15447df3de9f
Yahweasel <git-rpo@yahweasel.com>
2024-09-28 16:53:40 -0400

    Left an old version of the initializer lying around...

--- src/sem_init.c
+++ src/sem_init.c
@@ -17,7 +17,7 @@
 #include "../include/semaphore.h"
 
 int sem_init(sem_t *sem, int shared, unsigned int value) {
-    sem_t init = { PTHREAD_MUTEX_INITIALIZER, NULL, 0 };
+    sem_t init = { NULL, 0 };
 
     (void) shared;
     init.value = value;
